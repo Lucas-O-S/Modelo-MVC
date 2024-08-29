0b5e245b015f0905acd65107859ede04f3ad97d7
Lucas-O-S <lucasoscomercial@gmail.com>
2024-08-29 13:32:12 -0300

    HelperDAO adicionado

---
+++ MVCJogos/MVCJogos/DAO/HelperDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCJogos.DAO
+{
+    public static class HelperDAO
+    {
+        public static void ExecutarSQL(string sql, SqlParameter[] parametros)
+        {
+            using (SqlConnection conexao = ConexãoBD.GetConexao())
+            {
+                using(SqlCommand comando = new SqlCommand(sql, conexao))
+                {
+                    if(parametros != null)
+                    {
+                        comando.Parameters.AddRange(parametros);
+                        comando.ExecuteNonQuery();
+                    }
+                }
+            }
+
+        }
+    }
+}
